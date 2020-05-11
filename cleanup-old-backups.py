aa13d6911021c4f2067112f8d12241279aa33074
Alin Dobre <alin.dobre@outlook.com>
2020-05-11 07:30:48 +0100

    Add docroot files to cleanup process
    Signed-off-by: Alin Dobre <alin.dobre@outlook.com>

--- cleanup-old-backups.py
+++ cleanup-old-backups.py
@@ -37,6 +37,11 @@ if __name__ == '__main__':
     listing = onedrive_cli.onedrive_list(folder)
     destinations = {}
     for item in listing:
+        if item.startswith('docroot-') and item.endswith('.vcdiff'):
+            if not item.startswith('docroot-' + datetime.date.today().strftime('%Y-%V-%u')):
+                if item[:15] not in destinations:
+                    destinations[item[:15]] = []
+                destinations[item[:15]].append(item)
         if item.startswith('mysql-') and item.endswith('.vcdiff'):
             if not item.startswith('mysql-' + datetime.date.today().strftime('%Y-%V-%u')):
                 if item[:13] not in destinations:
@@ -48,4 +53,6 @@ if __name__ == '__main__':
         else:
             folder_id = listing[dest]
         for item in destinations[dest]:
+            if verbose:
+                print(f'{item} -> {dest}')
             onedrive_cli.onedrive_move(listing[item], folder_id)
