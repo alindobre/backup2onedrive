80fe6f827a96ff01ff8a656e3e951dea77ce08c7
Alin Dobre <alin.dobre@outlook.com>
2020-08-08 13:16:30 +0100

    Add support for custom config path and multiple remote folders
    Signed-off-by: Alin Dobre <alin.dobre@outlook.com>

--- cleanup-old-backups.py
+++ cleanup-old-backups.py
@@ -23,6 +23,8 @@ if __name__ == '__main__':
         sys.argv.remove('--debug')
 
     config = json.load(open(os.sep.join([os.environ['HOME'], 'backup.json'])))
+    if len(sys.argv) > 1:
+        config = json.load(open(sys.argv[1]))
     onedrive_cli.access_token = onedrive_cli.get_access_token(
         'https://login.live.com/oauth20_token.srf',
         config['onedrive']['client_id'],
@@ -30,34 +32,44 @@ if __name__ == '__main__':
         config['onedrive']['refresh_token']
     )
 
-    folder = None
+    folders = []
     if 'root_folder' in config['onedrive']:
-        folder = config['onedrive']['root_folder']
-
-    listing = onedrive_cli.onedrive_list(folder)
-    destinations = {}
-    for item in listing:
-        if item.startswith('docroot-') and item.endswith('.vcdiff'):
-            if not item.startswith('docroot-' + datetime.date.today().strftime('%Y-%V-%u')):
-                if item[:15] not in destinations:
-                    destinations[item[:15]] = []
-                destinations[item[:15]].append(item)
-        if item.startswith('mysql-') and item.endswith('.vcdiff'):
-            if not item.startswith('mysql-' + datetime.date.today().strftime('%Y-%V-%u')):
-                if item[:13] not in destinations:
-                    destinations[item[:13]] = []
-                destinations[item[:13]].append(item)
-        if item.startswith('mysql-') and item.endswith('.dump.xz'):
-            if not item.startswith('mysql-' + datetime.date.today().strftime('%Y-%V')):
-                if item[:13] not in destinations:
-                    destinations[item[:13]] = []
-                destinations[item[:13]].append(item)
-    for dest in destinations:
-        if dest not in listing:
-            folder_id = onedrive_cli.onedrive_mkdir(folder, dest)
+        if (type(config['onedrive']['root_folder']) == list):
+            folders.extend(config['onedrive']['root_folder'])
+        elif (type(config['onedrive']['root_folder']) == str):
+            folders = [ config['onedrive']['root_folder'] ]
         else:
-            folder_id = listing[dest]
-        for item in destinations[dest]:
-            if verbose:
-                print(f'{item} -> {dest}')
-            onedrive_cli.onedrive_move(listing[item], folder_id)
+            sys.stderr.write('ERROR: the root_folder JSON object is neither string nor list')
+            exit(1)
+    else:
+        sys.stderr.write('ERROR: could not find the root_folder JSON object')
+        exit(1)
+
+    for folder in folders:
+        listing = onedrive_cli.onedrive_list(folder)
+        destinations = {}
+        for item in listing:
+            if item.startswith('docroot-') and item.endswith('.vcdiff'):
+                if not item.startswith('docroot-' + datetime.date.today().strftime('%Y-%V-%u')):
+                    if item[:15] not in destinations:
+                        destinations[item[:15]] = []
+                    destinations[item[:15]].append(item)
+            if item.startswith('mysql-') and item.endswith('.vcdiff'):
+                if not item.startswith('mysql-' + datetime.date.today().strftime('%Y-%V-%u')):
+                    if item[:13] not in destinations:
+                        destinations[item[:13]] = []
+                    destinations[item[:13]].append(item)
+            if item.startswith('mysql-') and item.endswith('.dump.xz'):
+                if not item.startswith('mysql-' + datetime.date.today().strftime('%Y-%V')):
+                    if item[:13] not in destinations:
+                        destinations[item[:13]] = []
+                    destinations[item[:13]].append(item)
+        for dest in destinations:
+            if dest not in listing:
+                folder_id = onedrive_cli.onedrive_mkdir(folder, dest)
+            else:
+                folder_id = listing[dest]
+            for item in destinations[dest]:
+                if verbose:
+                    print(f'{folder}/{item} -> {dest}')
+                onedrive_cli.onedrive_move(listing[item], folder_id)
